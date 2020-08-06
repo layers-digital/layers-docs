22f7edbf56521ea519219cf62ef6b87395d538f3
Gabriel Raniere <gabrielraniere@MacBook-Air-do-Gabriel.local>
2020-08-06 11:11:24 -0300

    make each persmission into a separate code tag

--- src/components/openapi/authentication.tsx
+++ src/components/openapi/authentication.tsx
@@ -9,7 +9,7 @@ export class DocsOpenapiPermissions{
 
   resolveType(security){
     if(security.hasOwnProperty('Bearer') ){
-    return security["x-layers-permissions"] ?  <div>Autenticação do tipo <strong>Token Bearer</strong> é necessária para acessar essa rota. O app deve estar cadastrado para usar a API com {security["x-layers-permissions"].length > 1 ? 'as permissões' : 'a permissão'} <code>{security["x-layers-permissions"].length > 1 ? security["x-layers-permissions"].join(", ") : security["x-layers-permissions"]}</code></div> : <div>Autenticação do tipo <strong>Token Bearer</strong> é necessária para acessar essa rota.</div>
+    return security["x-layers-permissions"] ?  <div>Autenticação do tipo <strong>Token Bearer</strong> é necessária para acessar essa rota. O app deve estar cadastrado para usar a API com {security["x-layers-permissions"].length > 1 ? 'pelo menos uma das seguintes permissões:' : 'a permissão'} {security["x-layers-permissions"].length > 1 ? security["x-layers-permissions"].map((permission) => <code style={{"margin-right": "0.4em"}}key="permission">{permission}</code>) : <code>{security["x-layers-permissions"]}</code>}</div> : <div>Autenticação do tipo <strong>Token Bearer</strong> é necessária para acessar essa rota.</div>
     } else if (security.hasOwnProperty('OAuth2')){
       return <div>Autenticação do tipo <strong>OAuth 2.0</strong> é necessária para acessar essa rota. Para acessar essa rota o app deve estar cadastrado para usar a função de OAuth e ter ao menos um dos seguintes escopos: {security["OAuth2"].join(", ")}</div>
     } else {

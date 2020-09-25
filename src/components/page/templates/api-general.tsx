bd6bde104c93d9d2fdee8ef8493f2b179e41a14a
FeoSilva <felipeo.silva@hotmail.com>
2020-09-25 16:05:54 -0300

    Add API index page

---
+++ src/components/page/templates/api-general.tsx
@@ -0,0 +1,32 @@
+import { h } from '@stencil/core';
+import { toHypertext } from '../to-hypertext';
+import Template from '../../menu/templates/api';
+
+export default props => {
+  const { page } = props;
+
+  return (
+    <article>
+      {page.renderTitle !== false ? <h1>{page.title}</h1> : null}
+      {page.tableOfContents !== false ? (
+        <div class="page-meta">
+          <docs-table-of-contents links={page.headings} basepath={page.path}/>
+        </div>
+      ) : null}
+
+      <section class="markdown-content">
+        {toHypertext(h, page.body)}
+        <docs-cards>
+          {
+            Template.categories.filter(c => c.link != '/docs/api').map((category) => (
+              <docs-card header={category.title} href={category.link} icon={category.icon}>
+                <p>{category.description}</p>
+              </docs-card>
+            ))
+          }
+        </docs-cards>
+      </section>
+      <docs-page-footer page={page} />
+    </article>
+  );
+};
--- src/components/page/templates/index.ts
+++ src/components/page/templates/index.ts
@@ -3,11 +3,13 @@ import error from './error';
 import iframe from './iframe';
 import apiRoute from './api-route';
 import apiSchema from './api-schema';
+import apiGeneral from './api-general';
 
 export default {
   'default': defaultTemplate,
   'api-route': apiRoute,
   'api-schema': apiSchema,
+  'api-general': apiGeneral,
   'error': error,
   'iframe': iframe,
 };

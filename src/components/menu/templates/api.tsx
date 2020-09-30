9e9e5db1cc5bef4a865f9f71e0739ea4b5730972
Rafael Telles <rafael@telles.dev>
2020-09-30 12:19:12 -0300

    Revert "Merge branch 'master' of https://github.com/layers-digital/layers-docs"
    This reverts commit 9f6fc98e58f7b8ad02f487b38494197bd9657b2d, reversing
    changes made to 826cbe18d1236c729789f6f2feed19938402b5e6.

--- src/components/menu/templates/api.tsx
+++ src/components/menu/templates/api.tsx
@@ -5,54 +5,50 @@ import menuData from '../data/apis.json'
 const Template = {
   menu: {
     "general": {
-      'Menu': {
-        'Visão geral': '/docs/api',
+      'Grupo': {
+        'article 1': '/docs/api',
+        'article 2': '/docs/api',
       }
     },
     ...menuData,
   },
 
   template(props: RouteRenderProps) {
-    const navItems = Template.menu[props.match.params.service] ?? Template.menu['general']
+    const navItems = Template.menu[props.match.params.service] ?? Template.menu['auth']
     return [
       <docs-nav items={navItems} />,
     ]
   },
 
   categories: [
-    {
-      icon: '/docs/assets/icons/General.svg',
-      link: '/docs/api',
-      url: '/docs/api/index',
-      title: 'Guias Gerais',
-    },
+    // {
+    //   icon: '/docs/assets/icons/General.svg',
+    //   url: '/docs/api/intro',
+    //   title: 'Guias Gerais',
+    // },
     {
       icon: '/docs/assets/icons/OAuth.svg',
       url: '/docs/api/auth',
       link: '/docs/api/auth/oauth/account/info',
       title: 'Autenticação OAuth',
-      description: 'Autenticar usuários com o Layers e acessar informações com seu consentimento'
     },
     {
       icon: '/docs/assets/icons/Data.svg',
       url: '/docs/api/data',
       link: '/docs/api/data/member/object',
-      title: 'Dados',
-      description: 'Obter acesso aos dados de usuários, grupos, membros e mais'
+      title: 'Dados'
     },
     {
       icon: '/docs/assets/icons/Hub.svg',
       url: '/docs/api/apihub/',
       link: '/docs/api/apihub/@layerscalendargetrelated/post',
-      title: 'Hub de API\'s',
-      description: 'Prover informações sobre horários, frequência, cobranças e mais'
+      title: 'Hub de API\'s'
     },
     {
       icon: '/docs/assets/icons/Notifications.svg',
       url: '/docs/api/notifications',
       link: '/docs/api/notifications/notication/send/post',
-      title: 'Notificações',
-      description: 'Mandar notificações push para Android, iOS e Web'
+      title: 'Notificações'
     },
     // {
     //   icon: '/docs/assets/icons/E-commerce.svg',
@@ -64,22 +60,19 @@ const Template = {
       icon: '/docs/assets/icons/Comunicação.svg',
       url: '/docs/api/communication',
       link: '/docs/api/communication/event/object',
-      title: 'Comunicação',
-      description: 'Enviar comunicados para a comunidade escolar'
+      title: 'Comunicação'
     },
     {
       icon: '/docs/assets/icons/Atendimento.svg',
       url: '/docs/api/tickets',
       link: '/docs/api/tickets/channel/object',
-      title: 'Atendimentos',
-      description: 'Abrir chamados no Help Desk da comunidade escolar'
+      title: 'Atendimentos'
     },
     {
       icon: '/docs/assets/icons/Mídia.svg',
       url: '/docs/api/media',
       link: '/docs/api/media/media/upload/post',
-      title: 'Mídia',
-      description: 'Fazer upload de arquivos'
+      title: 'Mídia'
     },
     // {
     //   icon: '/docs/assets/icons/Payments.svg',

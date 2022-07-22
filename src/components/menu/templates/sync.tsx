4b301f50570b10ad84257865a1263e7f71f199b2
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-07-22 12:10:55 -0300

    change menu structure on data sync

--- src/components/menu/templates/sync.tsx
+++ src/components/menu/templates/sync.tsx
@@ -5,9 +5,43 @@ export default () => {
   return <docs-nav items={items} />
 };
 
+const menuStructure = {
+  "Entidades": [],
+  "Consumindo Dados": [],
+  "Provendo Dados": [],
+}
+
+const tagTextToType = {
+  "{...}": "Entidades",
+  "GET": "Consumindo Dados",
+  "HEAD": "Consumindo Dados",
+  "POST": "Provendo Dados",
+  "PUT": "Provendo Dados",
+  "DELETE": "Provendo Dados",
+  "PATCH": "Provendo Dados",
+}
+
+Object.keys(menuData.data).map((key: string) => {
+  const items = menuData.data[key];
+  items.forEach((item: {
+    text: string,
+    href: string,
+    tag: {
+      color: string,
+      text: string,
+    }
+  }) => {
+    const tag = item.tag.text
+    const type = tagTextToType[tag]
+    if (type) {
+      menuStructure[type].push(item)
+    }
+  });
+});
+
 const items = {
   'Layers Data Sync': {
     'Introdução': '/docs/api/data',
   },
-  ...(menuData.data)
+  ...menuStructure
 }
